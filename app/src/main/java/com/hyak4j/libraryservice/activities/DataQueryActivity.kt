84c6a68445f2faf8f7b29df103170492a41319b5
hyak4j <65548056+hyak4j@users.noreply.github.com>
2024-01-21 22:06:38 +0800

    [新增] 以ISBN查詢書籍或以ID查詢讀者功能

--- app/src/main/java/com/hyak4j/libraryservice/activities/DataQueryActivity.kt
+++ app/src/main/java/com/hyak4j/libraryservice/activities/DataQueryActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.hyak4j.libraryservice.databinding.ActivityDataQueryBinding
 import com.hyak4j.libraryservice.db.AppDatabase
+import com.hyak4j.libraryservice.util.IsbnUtil
+import com.hyak4j.libraryservice.util.UserIdUtil
 import java.util.concurrent.Executors
 
 class DataQueryActivity : AppCompatActivity() {
@@ -75,6 +78,77 @@ class DataQueryActivity : AppCompatActivity() {
                 }
                 runOnUiThread {
                     txtResult.text = resultText
+                }
+            }
+        }
+
+        // 以ISBN查詢書籍或以ID查詢讀者
+        btnSearch.setOnClickListener {
+            val id = dataId.text.toString()
+            if (id == "") {
+                val alertDialog = AlertDialog.Builder(this)
+                alertDialog.setTitle("ID Error")
+                alertDialog.setMessage("ID cannot be empty.")
+                alertDialog.setCancelable(false)
+                alertDialog.setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+                alertDialog.create().show()
+                return@setOnClickListener
+            }
+
+            if (id[0].isLetter() && !UserIdUtil.checkUserID(id, this)) {
+                // 字母開頭，但不符合讀者ID規則
+                return@setOnClickListener
+            } else if (!id[0].isLetter() && !IsbnUtil.checkISBN(id, this)) {
+                // 非字母開頭，但不符合書籍ISBN規則
+                return@setOnClickListener
+            }
+
+            singleThreadExecutor.execute {
+                try {
+                    val db = AppDatabase.buildAppDatabase(this)
+                    val userDao = db.getUserDao()
+                    val bookDao = db.getBookDao()
+                    var resultText = ""
+                    if (id[0].isLetter()) {
+                        // 以ID查詢讀者
+                        val user = userDao.getUserById(id)
+                        if (user != null) {
+                            resultText += "UserName is ${user.name}\n"
+                            resultText += "UserID is ${user.id}\n"
+                            resultText += "The borrowing books of this user are:\n"
+
+                            var isBorrowingBooks = false
+                            val bookBorrowing = arrayOf(user.bookBorrowing1, user.bookBorrowing2, user.bookBorrowing3,
+                                user.bookBorrowing4, user.bookBorrowing5)
+                            for (i in bookBorrowing.indices) {
+                                // 確認讀者有無借閱中的書籍，每人最多5本
+                                if (bookBorrowing[i] != null) {
+                                    isBorrowingBooks = true
+                                    val bookFound = bookDao.getBookByISBN(bookBorrowing[i]!!)
+                                    resultText += "${bookFound.isbn} ${bookFound.bookName}\n"
+                                }
+                            }
+                            if (!isBorrowingBooks) {
+                                resultText += "none \n"
+                            }
+                        } else {
+                            resultText += "User not found"
+                        }
+                    } else {
+                        // 以ISBN查詢書籍
+                        val book = bookDao.getBookByISBN(id)
+                        resultText += if (book != null) {
+                            "${book.isbn} ${book.bookName}"
+                        } else {
+                            "Book not found"
+                        }
+                    }
+
+                    runOnUiThread {
+                        txtResult.text = resultText
+                    }
+                } catch (e: Exception) {
+                    txtResult.text = e.message
                 }
             }
         }
